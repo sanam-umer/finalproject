30e3acd8933ed776226725681139688bdc49745e
sanam-umer <sanamummer1@gmail.com>
2021-09-24 11:29:32 +0530

    first commit:v1

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ChatComponent } from './containers/chat/chat.component';
+
+
+import { UsernameComponent } from './components/username/username.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ChatComponent,
+    
+    UsernameComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
