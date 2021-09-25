6dbc78ecbc68f2d4a412c5a484f510065254ce39
sanam-umer <81857355+sanam-umer@users.noreply.github.com>
2021-09-25 18:20:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,19 +9,7 @@ const io = require('socket.io')(http, {
         origin: '*'
     }
 });
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-app.use(requireHTTPS);
-app.use(express.static(’./dist/<name-on-package.json>’));
-app.get(’/*’, function(req, res) {
-  res.sendFile(’index.html’, {root: 'dist/<name-on-package.json>/’}
-);
-});
+
 
 app.get('/',(req,res)=>{
     res.header('Access-Control-Allow-Origin','*');
