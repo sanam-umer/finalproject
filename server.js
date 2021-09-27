9676be7e514c8145780bc01f2a78ea3e9c9b9380
sanam-umer <81857355+sanam-umer@users.noreply.github.com>
2021-09-27 20:19:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,4 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
+
 const cors = require('cors');
 const port = process.env.PORT || 3000;
 const app = express();
